0e12d9fe2f8d4a874fa43438359d0b509fd44967
Pierre H. Lehnen <Hudell@users.noreply.github.com>
2015-06-11 13:48:50 -0300

    Update effectus.rb

--- standalone/movement/compatibility/effectus.rb
+++ standalone/movement/compatibility/effectus.rb
@@ -1,13 +1,1 @@
-class Game_Player < Game_Character
-  def do_actual_start_map_event(block_x, block_y, triggers, normal)
-    return if $game_map.interpreter.running?
-    return if is_tile_checked?(block_x, block_y)
-
-    $game_map.ms_effectus_event_pos[y * $game_map.width + x].each do |event|
-      if event.trigger_in?(triggers) && event.normal_priority? == normal
-        mark_tile_as_checked(event.x, event.y) if event.trigger == 1 || event.trigger == 2
-        event.start
-      end
-    end
-  end
-end
+#Effectus no longer requires a compatibility patch.
